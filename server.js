d8d9078bb09543ddfd73496e1c5e4377ae9cc8fb
Wes Young <wes.young14@gmail.com>
2016-10-16 09:27:17 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+var port = process.env.port || 3000;
+
+app.listen(port, function () {
+  console.log('App listening on port ' + port);
+});
